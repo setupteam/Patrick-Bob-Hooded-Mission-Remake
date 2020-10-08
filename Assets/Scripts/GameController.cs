cae9548289aa7b78cb9d0fee52c176cf158fbe58
luE11 <luisedu554@hotmail.com>
2020-10-07 22:13:04 -0500

    Movement script added into player hooded
    Other changes:
    -Riggidbody2d added
    -New animator states added
    -etc :v

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { Idle, Playing, Ended, ReadyToRestart};
+
+public class GameController : MonoBehaviour
+{   
+    // Start is called before the first frame update
+    public int number = 0;
+    private GameState gameState = GameState.Idle;
+    void Start()
+    {
+        gameState = GameState.Playing;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        number++;
+    }
+}
