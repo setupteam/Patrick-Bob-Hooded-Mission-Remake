43024ac8a08597c533b7f93a041ca6cb4a2d52f8
Andrés Felipe Chaparro <andres.chaparro06@uptc.edu.co>
2020-10-14 09:03:00 -0500

    Otra propuesta para control
    Movimiento de hooded

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+	const int LEFT_DIRECTION = -1;
+	const int RIGHT_DIRECTION = 1;
     private bool isAlive;
-    private int direction = 0;
     public GameObject game, playerPrefab;
+    private int direction;
     private Animator animator;
     private float startY;
     private Rigidbody2D rb2d;
     public float speed;
+    public float jumpForce;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,7 @@ public class PlayerController : MonoBehaviour
         animator = playerPrefab.GetComponent<Animator>();
         isAlive = true;
         startY = transform.position.y;
+        this.direction = RIGHT_DIRECTION;
     }
 
     // Update is called once per frame
@@ -26,43 +30,51 @@ public class PlayerController : MonoBehaviour
         bool isGrounded = transform.position.y == startY;
         //bool isPlaying = (game.GetComponent<GameController>().gameState == GameState.Playing);
         bool isPlaying = true;
+        
         if(isPlaying){
             float moveHorizontal = Input.GetAxis ("Horizontal");
             float moveVertical = Input.GetAxis ("Vertical");
-            Vector2 movement = new Vector2 (moveHorizontal, (rb2d.velocity.y==0)?moveVertical*30f:0f); 
-            rb2d.AddForce (movement * speed);
-            
-            if(rb2d.velocity.x == 0 && rb2d.velocity.y==0){
-                UpdateState("hooded_Idle");
-            }else if(Input.GetKeyDown(KeyCode.LeftArrow)) {
-                if(direction==0){                   
-                    transform.Rotate(0f, 180f, 0f);
-                    direction=1;
+
+            if(isGrounded) {
+                rb2d.velocity = new Vector2(moveHorizontal * speed / 0.8f, rb2d.velocity.y);
+                if(Input.GetKey(KeyCode.UpArrow) && rb2d.velocity.y == 0){
+                    UpdateState("hooded_Idle");
+                    UpdateState("hooded_Jump");
+                    rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                }else if(Input.GetKey(KeyCode.LeftArrow)) {
+                    if(!Input.GetKey(KeyCode.RightArrow)){
+                        if(direction == RIGHT_DIRECTION){
+                            transform.rotation = Quaternion.Euler(0, 180, 0);
+                            direction = LEFT_DIRECTION;
+                        }
+                    }
+                }else if(Input.GetKey(KeyCode.RightArrow)) {
+                    if(direction == LEFT_DIRECTION){
+                        transform.rotation = Quaternion.Euler(0, 0, 0);
+                        direction = RIGHT_DIRECTION;
+                    }
                 }
-                if(rb2d.velocity.y==0 && rb2d.velocity.x != 0){
+
+                if(rb2d.velocity.x != 0){
                     UpdateState("hooded_Walk");
+                }else{
+                    UpdateState("hooded_Idle");
                 }
-            }else if(Input.GetKeyDown(KeyCode.RightArrow)) {
-                if(direction==1){
-                    transform.Rotate(0f, 180f, 0f);
-                    direction=0;
+            }else{
+                rb2d.velocity = new Vector2(moveHorizontal * speed, rb2d.velocity.y);
+                if(rb2d.velocity.y==0){
+                    transform.position = new Vector2(transform.position.x,startY);
+                }else if(rb2d.velocity.y < 0){
+                }else{
+                     UpdateState("hooded_Jump");
                 }
-                if(rb2d.velocity.y==0 && rb2d.velocity.x != 0){
-                    UpdateState("hooded_Walk");
-                }
-            }else if(Input.GetKeyDown(KeyCode.UpArrow) || rb2d.velocity.y!=0) {
-                UpdateState("hooded_Jump");
             }
-            if(rb2d.velocity.x != 0 && rb2d.velocity.y==0){
-                UpdateState("hooded_Walk");
-            }                 
         }   
     }
 
     public void UpdateState(string state = null)
     {
-        if (state != null)
-        {
+        if (state != null){
             animator.Play(state);
         }
     }
